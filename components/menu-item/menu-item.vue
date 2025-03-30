589604844c2c771901ae1e7b02f8fc7df20d6827
huangyuhui <theliarof.me@qq.com>
2025-03-30 21:01:13 +0800

    feat：新增联系客服；引入unplugin-auto-import插件 upd：调整菜单内容与传参

--- components/menu-item/menu-item.vue
+++ components/menu-item/menu-item.vue
@@ -1,23 +1,48 @@
 <template>
 	<view class="menu-item">
 		<view class="left">
-			<uni-icons type="download-filled" size="20" color="#28b389"></uni-icons>
+			<uni-icons :type="icon" size="20" color="#28b389"></uni-icons>
 			<view class="text">
-				我的下载
+				{{ name }}
 			</view>
 		</view>
 		<view class="right">
 			<view class="text">
-				0
+				{{ count }}
 			</view>
 			<uni-icons type="right" size="18" color="#aaa"></uni-icons>
-
 		</view>
+		<!-- #ifdef MP -->
+		<button v-if="type==='contact'" class="contact" open-type="contact">联系客服</button>
+		<!--  #endif -->
+		<!-- #ifndef MP -->
+		<button v-if="type==='contact'" class="contact" @click="onContact">拨打电话</button>
+		<!-- #endif -->
 	</view>
 </template>
 
 <script setup>
+	defineProps({
+		icon: {
+			type: String,
+		},
+		name: {
+			type: String,
+		},
+		count: {
+			type: Number,
+		},
+		type: {
+			type: String,
+			default: ''
+		}
+	})
 
+	function onContact() {
+		uni.makePhoneCall({
+			phoneNumber: '114'
+		})
+	}
 </script>
 
 <style lang="scss" scoped>
@@ -28,16 +53,12 @@
 		padding: 0 30rpx;
 		height: 100rpx;
 		border-bottom: 1rpx solid #eee;
-
-		&:last-child {
-			border-bottom: 0;
-		}
-
-
+		position: relative;
 
 		.left {
 			display: flex;
 			align-items: center;
+
 			.text {
 				padding-left: 10rpx;
 				color: #666;
@@ -47,11 +68,21 @@
 		.right {
 			display: flex;
 			align-items: center;
+
 			.text {
 				font-size: 28rpx;
 				padding-right: 10rpx;
 				color: #aaa;
 			}
+		}
+
+		.contact {
+			position: absolute;
+			top: 0;
+			left: 0;
+			height: 100%;
+			width: 100%;
+			opacity: 0;
 		}
 	}
 </style>
--- pages/user/user.vue
+++ pages/user/user.vue
@@ -13,19 +13,40 @@
 		</view>
 		<view class="section">
 			<view class="list">
-				<menu-item v-for="item in 3"></menu-item>
+				<menu-item v-for="({icon, name, count, type},index) in menu1" :icon="icon" :name="name" :count="count"
+					:type="type"></menu-item>
+
 			</view>
 		</view>
 		<view class="section">
 			<view class="list">
-				<menu-item v-for="item in 2"></menu-item>
+				<menu-item v-for="({icon, name},index) in menu2" :icon="icon" :name="name"></menu-item>
 			</view>
 		</view>
 	</view>
 </template>
 
 <script setup>
-
+	const menu1 = ref([{
+		icon: 'download-filled',
+		name: '我的下载',
+		count: 0
+	}, {
+		icon: 'star-filled',
+		name: '我的评分',
+		count: 0
+	}, {
+		icon: 'chatboxes-filled',
+		name: '联系客服',
+		type: 'contact'
+	}, ])
+	const menu2 = ref([{
+		icon: 'notification-filled',
+		name: '订阅更新'
+	}, {
+		icon: 'flag-filled',
+		name: '常见问题'
+	}, ])
 </script>
 
 <style lang="scss" scoped>
