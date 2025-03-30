8ce079adc930d4f00425e1e4ca82e2cef5f99149
huangyuhui <theliarof.me@qq.com>
2025-03-30 17:57:46 +0800

    feat：新增“分类”、“我的” 导航页

---
+++ components/menu-item/menu-item.vue
@@ -0,0 +1,57 @@
+<template>
+	<view class="menu-item">
+		<view class="left">
+			<uni-icons type="download-filled" size="20" color="#28b389"></uni-icons>
+			<view class="text">
+				我的下载
+			</view>
+		</view>
+		<view class="right">
+			<view class="text">
+				0
+			</view>
+			<uni-icons type="right" size="18" color="#aaa"></uni-icons>
+
+		</view>
+	</view>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+	.menu-item {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		padding: 0 30rpx;
+		height: 100rpx;
+		border-bottom: 1rpx solid #eee;
+
+		&:last-child {
+			border-bottom: 0;
+		}
+
+
+
+		.left {
+			display: flex;
+			align-items: center;
+			.text {
+				padding-left: 10rpx;
+				color: #666;
+			}
+		}
+
+		.right {
+			display: flex;
+			align-items: center;
+			.text {
+				font-size: 28rpx;
+				padding-right: 10rpx;
+				color: #aaa;
+			}
+		}
+	}
+</style>
---
+++ pages/user/user.vue
@@ -0,0 +1,74 @@
+<template>
+	<view class="user-layout">
+		<view class="user-info">
+			<view class="avator">
+				<image src="/static/images/logo.png" mode="aspectFill"></image>
+			</view>
+			<view class="nickname">
+				落日繁星
+			</view>
+			<view class="address">
+				来自于：福建
+			</view>
+		</view>
+		<view class="section">
+			<view class="list">
+				<menu-item v-for="item in 3"></menu-item>
+			</view>
+		</view>
+		<view class="section">
+			<view class="list">
+				<menu-item v-for="item in 2"></menu-item>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+	.user-layout {
+		.user-info {
+			display: flex;
+			align-items: center;
+			justify-content: center;
+			flex-direction: column;
+			padding: 50rpx 0;
+
+			.avator {
+				width: 160rpx;
+				height: 160rpx;
+				border-radius: 50%;
+				overflow: hidden;
+
+				image {
+					width: 100%;
+					height: 100%;
+				}
+			}
+
+			.nickname {
+				font-size: 44rpx;
+				color: #333;
+				padding: 20rpx 0 5rpx;
+			}
+
+			.address {
+				font-size: 28rpx;
+				color: #ababab;
+			}
+		}
+
+		.section {
+			width: 690rpx;
+			margin: 50rpx auto;
+			border: 1rpx solid #eee;
+			border-radius: 10rpx;
+			box-shadow: 0 0 30rpx rgba(0, 0, 0, 0.05);
+
+			.list {}
+		}
+	}
+</style>
