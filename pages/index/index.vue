4890c531c14548101ff1c72718d9ff2e78c9cd64
huangyuhui <theliarof.me@qq.com>
2025-03-30 10:33:08 +0800

    feat：新增banner

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,6 +1,13 @@
 <template>
 	<view class="home-layout">
-
+		<view class="banner">
+			<swiper indicator-dots autoplay :interval="3000" :duration="1000" indicator-color="rgba(255,255,255,0.5)"
+				indicator-active-color="#fff">
+				<swiper-item v-for="item in 3">
+					<image :src="`/common/images/banner${item}.jpg`" mode="aspectFill"></image>
+				</swiper-item>
+			</swiper>
+		</view>
 	</view>
 </template>
 
@@ -9,4 +16,21 @@
 </script>
 
 <style lang="scss" scoped>
+	.home-layout {
+		.banner {
+			width: 750rpx;
+
+			swiper {
+				width: 700rpx;
+				height: 340rpx;
+				padding: 20rpx 25rpx;
+
+				image {
+					width: 100%;
+					height: 100%;
+					border-radius: 10px;
+				}
+			}
+		}
+	}
 </style>
